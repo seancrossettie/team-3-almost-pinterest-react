f2693dbd4bbf8753cc134b4208b62b085d068535
Sean <jb@JBs-MacBook-Pro.local>
2021-05-10 20:49:23 -0500

    fixed errors

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import './styles/index.scss';
-<<<<<<< HEAD
-=======
-import App from './App';
->>>>>>> main
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import firebaseConfig from './helpers/apiKeys';

80958defe35160cbc8903d12e6409c153da723ed
Chie Stroud <urirunrunkoko@gmail.com>
2021-05-11 20:22:53 -0500

    forgot to add button to go back to boards page

--- src/views/SingleBoard.js
+++ src/views/SingleBoard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { useParams } from 'react-router-dom';
-import { Card, CardBody } from 'reactstrap';
+import { useParams, Link } from 'react-router-dom';
+import { Card, CardBody, Button } from 'reactstrap';
 import { getSingleBoard } from '../helpers/data/data';
 import { getBoardPins } from '../helpers/data/pinData';
 
@@ -15,10 +15,9 @@ export default function SingleBoard() {
     getBoardPins(id).then((data) => setPinBoard(data));
   }, []);
 
-  console.warn(pinBoard);
-
   return (
     <div>
+      <Button tag={Link} to='/boards' color="info">Go Back to All Boards</Button>
       <h1>Pin belongs to {board.boardTitle} board</h1>
       {pinBoard.map((pinBoardArray) => (
         <Card key={pinBoardArray.firebaseKey}>

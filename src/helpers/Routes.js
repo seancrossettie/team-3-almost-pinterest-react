7cd3f2a1566c607a46ce1f8624f3e3117baab4a6
Chie Stroud <urirunrunkoko@gmail.com>
2021-05-14 20:29:12 -0500

    added PrivateRoute to boards

--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -50,8 +50,11 @@ export default function Routes({
         path='/board/:id'
         user={user}
         component={() => <SingleBoard setPinBoard={setPinBoard}/>} />
-      <Route exact path='/boards/' component={() => <BoardView boards={boards} setBoards={setBoards} user={user}/>}/>
-      <Route exact path='/*' component={NotFound} />
+      <PrivateRoute
+        user={user}
+        path='/boards'
+        component={() => <BoardView boards={boards} setBoards={setBoards} user={user} />} />
+      <Route path='*' component={NotFound} />
     </Switch>
   );
 }
